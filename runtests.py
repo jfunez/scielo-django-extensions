ae5389728a16a89ff39b2cff986f1cd0d3475c4c
Juan Funez <juan.funez@gmail.com>
2014-03-31 17:08:06 -0300

    edito requirements e settings (do banco de dados) pora executar os test

--- runtests.py
+++ runtests.py
@@ -12,6 +12,16 @@ def main():
     )
     settings.PAGINATION__ITEMS_PER_PAGE = 5
     settings.DOCUMENTATION_BASE_URL = ''
+    settings.DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'scielo_extensions.db',         # Or path to database file if using sqlite3.
+            'USER': '',               # Not used with sqlite3.
+            'PASSWORD': '',                   # Not used with sqlite3.
+            'HOST': '',                       # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                       # Set to empty string for default. Not used with sqlite3.
+        }
+    }
 
     call_command('test_coverage', 'scielo_extensions')
 
